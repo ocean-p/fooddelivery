065db95f298df87e096995fd50245aa87c2d85e5
ocean-p <duongkodoiqua@gmail.com>
2022-06-27 21:27:20 +0700

    create header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Header } from './components';
+import { CreateContainer, Header, MainContainer } from './components';
 
 function App() {
   return (
-    <div className="w-screen h-auto flex flex-col">
+    <div className="w-screen h-auto flex flex-col bg-primary">
       <Header/>
+      
+      <main className='mt-24 p-8 w-full'>
+        <Routes>
+          <Route path='/*' element={<MainContainer/>} />
+          <Route path='/createItem' element={<CreateContainer/>} />
+        </Routes>
+      </main>
     </div>
   );
 }
